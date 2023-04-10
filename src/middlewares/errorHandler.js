053094d7075b3056b5fd61633dfe89ec0f68c12e
Luis Fernando <sloopy.max@gmail.com>
2023-04-10 15:03:23 -0600

    Display error when trying to delete resource referenced in another

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,7 +1,12 @@
 const errorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
   res.status(statusCode);
-  res.json({
+
+  if (err.errno === 1451) {
+   return res.json({message: "Comprueba que este recurso no tenga elementos asociados."}) 
+  }
+
+  return res.json({
     message: err.message,
     stack: process.env.NODE_ENV === "production" ? "❌" : err.stack,
   });
