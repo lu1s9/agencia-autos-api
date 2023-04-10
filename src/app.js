883e8fdb747a69b47aec64b486c419230193b839
Luis Fernando <sloopy.max@gmail.com>
2023-04-10 12:27:09 -0600

    Enable all CORS Requests

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import helmet from "helmet";
+import cors from "cors"
 import notFound from "./middlewares/notFound.js";
 import errorHandler from "./middlewares/errorHandler.js";
 import routes from "./routes/index.routes.js";
 
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 app.use(helmet());
 app.use("/", routes);
